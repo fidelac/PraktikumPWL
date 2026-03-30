137fcd3c19de22f194086433a5b107087e8f27e2
fidelac <clarisafidela@gmail.com>
2026-03-30 23:54:20 +0700

    update js 7

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -8,6 +8,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 
 class ProductInfolist
 {
@@ -15,7 +17,11 @@ class ProductInfolist
     {
         return $schema
             ->components([
-                Section::make('Product info')
+                Tabs::make('Product Tabs')
+                ->vertical()
+                ->tabs([
+                Tab::make('Product Details')
+                     ->icon('heroicon-o-academic-cap')
                     ->schema([
                         TextEntry::make('name')
                             ->label('Name')
@@ -33,12 +39,13 @@ class ProductInfolist
                             ->label('Created At')
                             ->dateTime('d M Y')
                             ->color('info'),
-
-                    ])->columnSpanFull(),
-
-                Section::make('Product Price & Stock')
-                    ->description('')
                     
+                    ]),
+
+                Tab::make('Product Price & Stock')  
+                    ->badge(fn ($record) => $record->stock)
+                    ->icon('heroicon-o-currency-dollar')
+                    ->badgeColor(fn ($record) => $record->stock > 10 ? 'success' : 'danger')
                     ->schema([
                         TextEntry::make('price')
                             ->label('Product Price')
@@ -49,11 +56,11 @@ class ProductInfolist
                         TextEntry::make('stock')
                             ->label('ProductStock')
                             ->icon('heroicon-o-cube'),
-                    ])
-                    ->columnSpanFull(),
+                    ]),
+            
 
-                Section::make('Image & Status')
-                    ->description('')
+                Tab::make('Image & Status')
+                     ->icon('heroicon-o-photo')
                     ->schema([
                         ImageEntry::make('image')
                             ->label('Product Image')
@@ -75,7 +82,58 @@ class ProductInfolist
                             ->label('Is Featured?')
                             ->boolean(),
                     ])
-                    ->columnSpanFull(),
+                ])  ->columnSpanFull()
+                    ->vertical(),
+                // Section::make('product information')
+                //     ->schema([
+                //         TextEntry::make('name')
+                //             ->label('Name')
+                //             ->weight('bold')
+                //             ->color('primary'),
+                //         TextEntry::make('id')
+                //             ->label('Product ID'),
+                //         TextEntry::make('sku')
+                //             ->label('Product SKU')
+                //             ->badge()
+                //             ->color('success'),
+                //         TextEntry::make('description')
+                //             ->label('Description'),
+                //         TextEntry::make('created_at')
+                //             ->label('Created At')
+                //             ->dateTime('d M Y')
+                //             ->color('info'),
+                //     ])->columnSpanFull(),
+                // Section::make('price & stock')
+                //     ->schema([
+                //         TextEntry::make('price')
+                //             ->label('Product Price')
+                //             ->weight('bold')
+                //             ->color('primary')
+                //             ->formatStateUsing(fn ($state) => 'Rp ' . number_format($state, 0, ',', '.'))
+                //             ->icon('heroicon-o-currency-dollar'),
+                //         TextEntry::make('stock')
+                //             ->label('ProductStock')
+                //             ->icon('heroicon-o-cube'),
+                //     ])->columnSpanFull(),
+                // Section::make('image & status')
+                //     ->schema([
+                //         ImageEntry::make('image')
+                //             ->label('Product Image')
+                //             ->disk('public'),
+                //         TextEntry::make('price')
+                //             ->label('Product Price')
+                //             ->weight('bold')
+                //             ->color('primary')
+                //             ->formatStateUsing(fn ($state) => 'Rp ' . number_format($state, 0, ',', '.'))
+                //             ->icon('heroicon-o-currency-dollar'),
+                //         TextEntry::make('stock')
+                //             ->label('ProductStock')
+                //             ->weight('bold')
+                //             ->color('primary'),
+                //         IconEntry::make('is_active')
+                //             ->label('Is Active?')
+                //             ->boolean(),        
+                //     ])->columnSpanFull(),
             ]);
     }
 }
